4a24a62027ba37da55105ce2ee867feac764b5e2
Xiaoyi Cao <x9cao@eng.ucsd.edu>
2018-11-30 16:39:04 -0800

    Final draft layout

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
 	if ($('.slickSlider').length) {
-      $('.slickSlider').slick({
-		  dots: true,
-		  zIndex: 9,
-		  prevArrow: '#mainSliderHolder .prevArrow',
-		  nextArrow: '#mainSliderHolder .nextArrow',
-		  autoplay: true,
-		  autoplaySpeed: 10000,
-		  lazyLoad: 'progressive',
-		  respondTo: 'min'
-	  });
+		$('.slickSlider').each(function (i) {
+			var prevArrow = $(this).parent().children('.prevArrow').length
+				? $(this).parent().children('.prevArrow')
+				: ($(this).parent().parent().children('.prevArrow').length
+					? $(this).parent().parent().children('.prevArrow')
+					: undefined);
+			var nextArrow = $(this).parent().children('.nextArrow').length
+				? $(this).parent().children('.nextArrow')
+				: ($(this).parent().parent().children('.nextArrow').length
+					? $(this).parent().parent().children('.nextArrow')
+					: undefined);
+			$(this).slick({
+				dots: true,
+				zIndex: 9,
+				prevArrow: prevArrow,
+				nextArrow: nextArrow,
+				autoplay: true,
+				autoplaySpeed: 10000,
+				lazyLoad: 'progressive',
+				respondTo: 'min',
+				variableWidth: false
+			});
+			$(this).removeClass('unresolved');
+		});
 	}
 });
